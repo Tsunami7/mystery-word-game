5b96b66dd6d77469cbceb90188370ffbcd3f4faf
Alexander Barbaran <abarba02@gmail.com>
2019-08-28 01:55:37 -0400

    hw

--- main.rb
+++ main.rb
@@ -1,10 +1,18 @@
 class Word
   # Stores the provided word in an instance variable and
   # setups up any other instance variables.
+  attr_accessor :word, :letter_counter, :guess_word
+
+
   def initialize(word)
     # puts "provide solution to the mystery word"
     # @word= gets.chomp
-    @word= word
+    @word = word
+    @letter_counter = 0
+    @guess_word = Array.new(word.length) {'_'}
+  end
+
+  #this is a getter so gets info
 
   # Guess a letter in the word.
   # Should return a boolean. true if the letter is in the word, false if not.
@@ -15,32 +23,46 @@ class Word
 
   # Whether all letters in the word have been guessed correctly.
   def guessed_all_correct?
-    newWord = word.split("")
-    
-    for 
+    if letter_counter == word.length
+      return true
+    end
+    false
   end
 
   # Display the current state of the guessed word for the player.
   def output_word
-    puts guess_word
+    print guess_word
   end
 
   # Check whether a provided character is a letter
   def self.is_letter?(character)
-    character.include?
+    alphabet = ('a'..'z').to_a
+
+    while !alphabet.include?(character)
+      puts "Invalid character. Please give a letter"
+      character = gets.chomp
+    end
+    character 
   end
 end
 
 class MysteryWordGame
+  attr_accessor :secret, :correct_word, :lives
   # Initialize any state in the game, and start the game loop.
   def initialize
     # begin the first phases
+    @secret=''
+    @correct_word = []
+    @lives = 3
   end
-
+  
   def ask_for_word
     # ask for word from player1 for the player2 to guess
     # What is the word? Player 2 look away...
     # dog
+    puts "Player 1 enter Secret word: "
+    @secret = Word.new(gets.chomp)
+    @correct_word = @secret.word.split("")
   end
 
   def ask_for_letter
@@ -49,22 +71,52 @@ class MysteryWordGame
     # o
     # Yup, the word includes that letter!
     # Oh no! The word doesn't include that letter!
+    puts "Player 2 enter letter:"
+    guessLetter = Word.is_letter?(gets.chomp)
+    if secret.guess?(guessLetter)
+      # enumerable do and end
+      correct_word.each_with_index do |letter, index|
+        if letter == guessLetter
+          secret.letter_counter += 1
+          secret.guess_word[index] = guessLetter #replaces '_' with guessLetter
+        end
+      end
+      puts "#{guessLetter} Yup, the word includes that letter!"
+    else
+      @lives -= 1
+      puts "#{guessLetter} Oh no! The word doesn't include that letter!"
+    end
+    
   end
 
   # Run the game loop, which continues until the player2 wins or loses.
   def game_loop
+    ask_for_word
+
+    while !(lives == 0 || secret.guessed_all_correct?)
+      ask_for_letter
+      secret.output_word
+
+      puts lives, "number of lives"
+      puts  secret.letter_counter, "Letter count"  
+    end
+
+    if lives == 0
+      puts "Game Over you lose"
+    elsif secret.guessed_all_correct?
+      puts "You Win!"
+    else
+      # raise error console.log
+      raise "Something went wrong!"
+    end
+
   end
 end
 
-MysteryWordGame.new
+hangman = MysteryWordGame.new
 
-# this will allow us to run the code
-# mystery_word = Word.new
-# new_word = mysteryWord
-# puts new
+hangman.game_loop
 
-#
-word.is_letter?
 
 
 
